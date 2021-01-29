96e5ebc0f5b452a218121d37cdee0d1cdaaa498e
Kuan-Wen Chen <kev888kev@gmail.com>
2021-01-29 15:13:52 +0800

    Create the listing page (part 2)

--- admin/pages/index.vue
+++ admin/pages/index.vue
@@ -21,21 +21,22 @@
         </div>
       </div>
     </div>
+    
     <div class="a-spacing-large"></div>
 
     <!-- Listing page -->
     <div class="container-fluid browsing-history">
       <div class="row">
-        <div class="col-xs-2 col-lg-2 col-mg-3 col-sm-6 br bb">
+        <div v-for="(product, index) in products" :key="product._id" class="col-xs-2 col-lg-2 col-mg-3 col-sm-6 br bb">
           <div class="history-box">
             <!-- Product image -->
             <a href="#" class="a-link-normal">
-              <img src="#" class="img-fluid" />
+              <img :src="product.photo" class="img-fluid" />
             </a>
             <!-- Product title -->
             <div class="a-spacing-top-base asin-title">
               <span class="a-text-normal">
-                <div class="p13n-sc-truncated">Title</div>
+                <div class="p13n-sc-truncated">{{product.title}}</div>
               </span>
             </div>
             <!-- Product rating -->
@@ -55,46 +56,7 @@
             <!-- Product price -->
             <div class="a-row">
               <span class="a-size-base a-color-price">
-                <span class="p13n-sc-price">Price</span>
-              </span>
-            </div>
-            <!-- Product Buttons -->
-            <div class="a-row">
-              <a class="a-button-history margin-right-10">Update</a>
-              <a href="#" class="a-button-history margin-right-10">Delete</a>
-            </div>
-          </div>
-        </div>
-        <div class="col-xs-2 col-lg-2 col-mg-3 col-sm-6 br bb">
-          <div class="history-box">
-            <!-- Product image -->
-            <a href="#" class="a-link-normal">
-              <img src="#" class="img-fluid" />
-            </a>
-            <!-- Product title -->
-            <div class="a-spacing-top-base asin-title">
-              <span class="a-text-normal">
-                <div class="p13n-sc-truncated">Title</div>
-              </span>
-            </div>
-            <!-- Product rating -->
-            <div class="a-row">
-              <a href="#">
-                <i class="fas fa-star"></i>
-                <i class="fas fa-star"></i>
-                <i class="fas fa-star"></i>
-                <i class="fas fa-star"></i>
-                <i class="fas fa-star"></i>
-              </a>
-              <span class="a-letter-space"></span>
-              <span class="a-color-tertiary a-size-small asin-reviews"
-                >(1732)</span
-              >
-            </div>
-            <!-- Product price -->
-            <div class="a-row">
-              <span class="a-size-base a-color-price">
-                <span class="p13n-sc-price">Price</span>
+                <span class="p13n-sc-price">${{product.price}}</span>
               </span>
             </div>
             <!-- Product Buttons -->
@@ -110,7 +72,19 @@
 </template>
 
 <script>
-export default {};
+export default {
+  async asyncData({$axios}){
+    try {
+      let response = await $axios.$get("http://localhost:3000/api/products")
+      console.log(response)
+      return {
+        products: response.products
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+};
 </script>
 
 <style></style>
