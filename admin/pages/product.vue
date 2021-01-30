d38ce3e23913473ef3958d4c8efec1b2f3267d59
Kuan-Wen Chen <kev888kev@gmail.com>
2021-01-30 16:55:36 +0800

    Finishing the product-adding page

--- admin/pages/product.vue
+++ admin/pages/product.vue
@@ -12,31 +12,60 @@
               <!-- Category dropdown -->
               <div class="a-spacing-top-medium">
                 <label>Category</label>
-                <select class="a-select-option">
-                  <option value="">1</option>
-                  <option value="">2</option>
+                <select class="a-select-option" v-model="categoryID">
+                  <option
+                    v-for="category in categories"
+                    :key="category._id"
+                    :value="category._id"
+                    >{{ category.type }}</option
+                  >
                 </select>
               </div>
 
               <!-- Owner dropdown -->
               <div class="a-spacing-top-medium">
                 <label>Owner</label>
-                <select class="a-select-option">
-                  <option value="">1</option>
-                  <option value="">2</option>
+                <select class="a-select-option" v-model="ownerID">
+                  <option
+                    v-for="owner in owners"
+                    :key="owner._id"
+                    :value="owner._id"
+                    >{{ owner.name }}</option
+                  >
                 </select>
               </div>
 
               <!-- Title-->
               <div class="a-spacing-top-medium">
                 <label style="margin-bottom: 0px">Title</label>
-                <input type="text" class="a-input-text" style="width: 100%" />
+                <input
+                  type="text"
+                  class="a-input-text"
+                  style="width: 100%"
+                  v-model="title"
+                />
               </div>
-              <!-- Price-->
 
+              <!-- Price-->
               <div class="a-spacing-top-medium">
                 <label style="margin-bottom: 0px">Price</label>
-                <input type="number" class="a-input-text" style="width: 100%" />
+                <input
+                  type="number"
+                  class="a-input-text"
+                  style="width: 100%"
+                  v-model="price"
+                />
+              </div>
+
+              <!-- stockQuantity-->
+              <div class="a-spacing-top-medium">
+                <label style="margin-bottom: 0px">Stock Quantity</label>
+                <input
+                  type="number"
+                  class="a-input-text"
+                  style="width: 100%"
+                  v-model="stockQuantity"
+                />
               </div>
 
               <!-- Description-->
@@ -45,6 +74,7 @@
                 <textarea
                   placeholder="Please provide some description"
                   style="width: 100%"
+                  v-model="description"
                 />
               </div>
 
@@ -54,8 +84,8 @@
                 <div class="a-row a-spacing-top-medium">
                   <label class="choosefile-button">
                     <i class="fal fa-plus"></i>
-                    <input type="file" />
-                    <p style="margin-top: -70px">photo name</p>
+                    <input type="file" @change="onFileSelected" />
+                    <p style="margin-top: -70px">{{ fileName }}</p>
                   </label>
                 </div>
               </div>
@@ -65,7 +95,9 @@
               <div class="a-spacing-large">
                 <span class="a-button-register">
                   <span class="a-button-inner">
-                    <span class="a-button-text">Add product</span>
+                    <span class="a-button-text" @click="onAddProduct"
+                      >Add product</span
+                    >
                   </span>
                 </span>
               </div>
@@ -80,5 +112,62 @@
 </template>
 
 <script>
-export default {};
+export default {
+  async asyncData({ $axios }) {
+    try {
+      let categories = $axios.$get("http://localhost:3000/api/categories");
+      let owners = $axios.$get("http://localhost:3000/api/owners");
+
+      const [catResponse, ownerResponse] = await Promise.all([
+        categories,
+        owners
+      ]);
+
+      return {
+        categories: catResponse.categories,
+        owners: ownerResponse.owners
+      };
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  data() {
+    return {
+      categoryID: null,
+      ownerID: null,
+      title: "",
+      price: 0,
+      description: "",
+      selectedFile: null,
+      stockQuantity: 1,
+      fileName: ""
+    };
+  },
+
+  methods: {
+    onFileSelected(event) {
+      this.selectedFile = event.target.files[0];
+      console.log(this.selectedFile);
+      this.fileName = event.target.files[0].name;
+    },
+
+    async onAddProduct() {
+      let data = new FormData();
+      data.append("title", this.title);
+      data.append("price", this.price);
+      data.append("description", this.description);
+      data.append("ownerID", this.ownerID);
+      data.append("stockQuantity", this.stockQuantity);
+      data.append("categoryID", this.categoryID);
+      data.append("photo", this.selectedFile, this.selectedFile.name);
+
+      let result = await this.$axios.$post(
+        "http://localhost:3000/api/products",
+        data
+      );
+
+      this.$router.push("/");
+    }
+  }
+};
 </script>
