382c2441a894a9aa33b5d9041930fee192c78f5b
Kuan-Wen Chen <kev888kev@gmail.com>
2021-01-31 22:59:43 +0800

    Add the FeaturedProduct component

---
+++ client/components/FeaturedProduct.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="mainContent">
+    <div class="unified_widget pageBanner">
+      <h1>Books</h1>
+
+      <p>
+        <b>
+          Browse for the
+          <a href="#">best fantasy books</a> like Game of thrones, or
+          <a href="#">best programming books of the year so far</a>
+        </b>
+      </p>
+    </div>
+
+    <div class="bx-root">
+      <div class="row">
+        <div class="col-sm-5 text-center">
+          <div class="bx-root-image">
+            <img src="/img/featuredProduct.jpeg" class="img-fluid" />
+          </div>
+        </div>
+        <div class="col-sm-7 m-auto">
+          <div class="bx-root-dtls">
+            <div class="bx-root-heading">Introduction to Algorithms</div>
+          </div>
+          <div class="bx-root-subtext">
+            Canonical books for all programmers
+          </div>
+          <div class="bx-root-link">
+            <span>Order Now</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,13 +1,25 @@
 <template>
-  <div class="container">
-    <NuxtLink to="/products" class="a-button-buy-again"
-      >Add a new product</NuxtLink
-    >
-  </div>
+  <main class="listingPage">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-xl-2 col-lg-3 md-4 col-sm-4">
+          <!--- Sidebar -->
+        </div>
+
+        <!--- Main Content --->
+        <div class="col-xl-10 col-lg-9 md-8 col-sm-8">
+          <featured-product />
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
 
 <script>
-export default {};
+import FeaturedProduct from "~/components/FeaturedProduct";
+export default {
+  components: {
+    FeaturedProduct
+  }
+};
 </script>
-
-<style></style>
