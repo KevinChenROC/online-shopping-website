a14402ab206804af47eb3cb6880ab688ee6d8aa7
Kuan-Wen Chen <kev888kev@gmail.com>
2021-01-31 19:41:13 +0800

    Initial config of the client app

---
+++ client/pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container">
+    <NuxtLink to="/products" class="a-button-buy-again"
+      >Add a new product</NuxtLink
+    >
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
