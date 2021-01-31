b7fd03466bbfd9a63b24c572acfd9b6a8afa63bc
Kuan-Wen Chen <kev888kev@gmail.com>
2021-01-31 22:01:42 +0800

    Add the footer component

--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -2,14 +2,17 @@
   <div>
     <NavBar></NavBar>
     <Nuxt />
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
 import NavBar from "~/components/NavBar";
+import Footer from "~/components/Footer";
 export default {
   components: {
-    NavBar
+    NavBar,
+    Footer
   }
 };
 </script>
