ade80031a35f56059244829ff7c3b7b2580be17a
Kuan-Wen Chen <kev888kev@gmail.com>
2021-02-01 18:08:00 +0800

    Move S3 Bucket name to .env

--- server/middlewares/upload-photo.js
+++ server/middlewares/upload-photo.js
@@ -12,7 +12,7 @@ s3 = new aws.S3();
 const upload = multer({
   storage: multerS3({
     s3: s3,
-    bucket: "amazon-clone-kevin",
+    bucket: process.env.S3_BUCKET_NAME,
     acl: "public-read",
     metadata: (req, file, cb) => {
       cb(null, { fieldName: file.fieldname });
